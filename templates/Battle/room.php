3eda467cba86983e7efa3dd7037519b044f50514
Kunugibayashi <nycticorax.nycticorax.62@gmail.com>
2025-01-13 11:20:48 +0900

    ver.3.0.27
    ・攻撃スキルとスキル位置が異なる場合、ボタンの色を変更する機能を追加

--- templates/Battle/room.php
+++ templates/Battle/room.php
@@ -47,9 +47,50 @@ jQuery(function(){
             button.text('攻撃実行');
         } else {
             button.removeClass('warning');
+            button.removeClass('mismatch');
             button.text('送信');
         }
     });
+    $("#is-attack-label, select[name=attack_skill_code], input[name=attack_skill_attribute]").on('change', function(){
+        var checkbox = jQuery('#id-is-attack');
+        var button = jQuery('#id-send-battle-message-button');
+        // チェックが入っていない場合は色を変えない
+        if (checkbox.prop('checked')){
+            // スキル位置のチェック
+            var attkSkillCode = $('select[name=attack_skill_code] option:selected').val(); // 攻撃スキルコード
+            var attkSkillAttribute = $('input[name=attack_skill_attribute]:checked').val(); // 攻撃スキル位置
+            if (attkSkillCode == '<?= BT_AT_SEI_01 ?>' && attkSkillAttribute != '<?= BT_ATTR_01 ?>') { // 精密射撃（炎）
+                button.removeClass('warning');
+                button.addClass('mismatch');
+            } else if (attkSkillCode == '<?= BT_AT_SEI_02 ?>' && attkSkillAttribute != '<?= BT_ATTR_02 ?>') { // 精密射撃（地）
+                button.removeClass('warning');
+                button.addClass('mismatch');
+            } else if (attkSkillCode == '<?= BT_AT_SEI_03 ?>' && attkSkillAttribute != '<?= BT_ATTR_03 ?>') { // 精密射撃（風）
+                button.removeClass('warning');
+                button.addClass('mismatch');
+            } else if (attkSkillCode == '<?= BT_AT_SEI_04 ?>' && attkSkillAttribute != '<?= BT_ATTR_04 ?>') { // 精密射撃（水）
+                button.removeClass('warning');
+                button.addClass('mismatch');
+            } else if (attkSkillCode == '<?= BT_AT_BUI_01 ?>' && attkSkillAttribute != '<?= BT_ATTR_01 ?>') { // 部位破壊（炎）
+                button.removeClass('warning');
+                button.addClass('mismatch');
+            } else if (attkSkillCode == '<?= BT_AT_BUI_02 ?>' && attkSkillAttribute != '<?= BT_ATTR_02 ?>') { // 部位破壊（地）
+                button.removeClass('warning');
+                button.addClass('mismatch');
+            } else if (attkSkillCode == '<?= BT_AT_BUI_03 ?>' && attkSkillAttribute != '<?= BT_ATTR_03 ?>') { // 部位破壊（風）
+                button.removeClass('warning');
+                button.addClass('mismatch');
+            } else if (attkSkillCode == '<?= BT_AT_BUI_04 ?>' && attkSkillAttribute != '<?= BT_ATTR_04 ?>') { // 部位破壊（水）
+                button.removeClass('warning');
+                button.addClass('mismatch');
+            } else {
+                button.removeClass('mismatch');
+                button.addClass('warning');
+            }
+        }
+        return false;
+    });
+
     jQuery('#id-reload-log-button').on('click', function(){
         jQuery('#id-chatentrieslist-reload').trigger('click');
         jQuery('#id-chatlog-reload').trigger('click');
@@ -89,6 +130,7 @@ jQuery(function(){
             if (checkbox.prop('checked')) {
                 checkbox.removeAttr('checked').prop('checked', false).change();
                 button.removeClass('warning');
+                button.removeClass('mismatch');
                 button.text('送信');
             }
 
